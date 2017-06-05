08df7a122c66008b4cdb350ae1b16a645ed02a8b
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-06-05 15:17:46 +0300

    add user to openLDAP

--- add_objectclass.py
+++ add_objectclass.py
@@ -7,6 +7,14 @@
 import ldap
 import ldap.modlist as modlist
 
+print("********************************")
+print("*                              *")
+print("*   Add pardusAccount and      *")
+print("*   pardusLider objectClass    *")
+print("*       to OpenLDAP            *")
+print("*                              *")
+print("********************************")
+
 hostname = raw_input("ldap sunucu ip adresini giriniz(192.168.56.1): ")
 search_base = raw_input("ldap search_base bilgisini giriniz(örn: dc=tuncay,dc=colak): ")
 # hostname="192.168.56.102"
@@ -14,7 +22,7 @@ search_base = raw_input("ldap search_base bilgisini giriniz(örn: dc=tuncay,dc=c
 base_dn = "cn=admin,"+str(search_base)
 pwd = raw_input("ldap admin parolasını giriniz: ")
 # pwd = "1"
-ldap_obj = ldap.open(hostname)
+ldap_obj = ldap.initialize(hostname)
 ldap_obj.simple_bind_s(base_dn,pwd)
 print ("ldap'a bağlantı kuruldu......")
 
